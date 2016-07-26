5d68028e22b3ac8c7764049940bcde6adbe39a84
Kyle Orlinski <orlinskikyle@gmail.com>
2016-07-25 23:35:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,19 +14,38 @@ $(document).ready(function() {
                    async: false,
                    dataType: 'json',
                    success: function(data, status, jqXHR){
-                        $(".responsecontainer").fadeOut(function(){
+                        $(".responsecontainer").fadeOut(0 ,function(){
                             $(".responsecontainer").html("<h1>You Searched ''" + data[0] + "'' ");
                             $(".responsecontainer h1").addClass("headingresult");
                             $(".responsecontainer").append('<ul class="resultformat">' + "</ul>");
                             for (var i = 0; i < data[1].length; i++) {
-                                $(".responsecontainer ul").append('<a href="'+ wikiSite + data[1][i] + '">' + "<li>" + "<h2>" + data[1][i] + "</h2>" + "<p>" + data[2][i] + "</p>" + "</li></a><br>");
+                                $(".responsecontainer ul").append('<a href="'+ wikiSite + data[1][i] + '">' + 
+                   "<li>" +
+                        '<div class="row">' + 
+                            '<div class="col-lg-4">' +
+                                "<h2>" + data[1][i] + "</h2>" +
+                            "</div>"
+                        +   '<div class="col-lg-8">' + 
+                                "<p>" + data[2][i] + "</p>" + 
+                            "</div>" + 
+                        "</div>" 
+                   + "</li></a><br>"); 
                             }
-                            
                         }); 
                         $(".responsecontainer").fadeIn();
-                   /*console.log(data);
-                     console.log(data[0]);
-                     console.log(data[1][0]);*/
+                        var styleListItem = document.querySelectorAll(".resultformat li");
+                        for (let i = 0; i < styleListItem.length; i++) {
+                            
+                        }
+                        /*$(".resultformat li").on("mouseover", function() {
+                            $(this).fadeIn(function() {
+                               $(".resultformat li").css({
+                                  "border-left": "solid 5px red" 
+                               }); 
+                            });
+                        });*/
+                        //$(".res")
+                        //http://ejohn.org/apps/learn/ javascript tutorials random note for later
                    },
                })
                .done(function() {
